4d9be40279eae5a5018dd0c333fce81f4b223f05
akimt <a.kimtsaris@outlook.com>
2020-11-24 15:32:03 +0100

    initial commit

--- src/main/java/bstrom/akimts/demoRestSpring/controller/HelloController.java
+++ src/main/java/bstrom/akimts/demoRestSpring/controller/HelloController.java
@@ -4,6 +4,7 @@ import bstrom.akimts.demoRestSpring.model.Personne;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import javax.websocket.server.PathParam;
 import java.util.Arrays;
 import java.util.List;
@@ -34,7 +35,7 @@ public class HelloController {
 
     // POST > http://localhost:8080/hello -> ecrit la chaine envoyé par post dans la console de l'appli
     @PostMapping
-    public void writeInConsole(@RequestBody Personne body){
+    public void writeInConsole(@Valid @RequestBody Personne body){
         System.out.println(body);
         // throw new RuntimeException();
     }
